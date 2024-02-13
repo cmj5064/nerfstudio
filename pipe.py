fb4d667ce1a5f1c216b8688edccf6bae51e67f0c
cmj5064 <duttu5064@naver.com>
2024-02-13 09:56:51 +0000

    feat: UXR web server와 통신 규격에 맞춰 추론 결과 전송 코드 추가

--- pipe.py
+++ pipe.py
@@ -3,6 +3,20 @@ import os
 import time
 from datetime import timedelta
 import json
+import requests
+from requests_toolbelt.multipart.encoder import MultipartEncoder
+
+
+def send(name, file, id):
+    url = "https://zzimkong.ggm.kr/inference/done"
+    ply_file = MultipartEncoder(
+        fields={
+            'file': (f'{name}.ply', open(file, 'rb'))
+            # 'file': (f'{name}.png', open(file, 'rb')) # NOTE request test
+        }
+    )
+    data = {"file": ply_file, "id": id}
+    r = requests.post(url, data=data, verify=False)
 
 
 def main(args):
@@ -13,8 +27,14 @@ def main(args):
 
     base = os.getcwd()                 # pwd output. ./nerfstudio
     model = args.model                 # nerfacto
-    data = args.data                   # room.mp4 saved in {base}/data
-    name_0 = data.split('.')[0]        # room
+    if "https:" in args.src:
+        data_url = args.src                # https://zzimkong.ggm.kr/2024.mov
+        data = data_url.split('/')[-1]     # room.mp4
+        name_0 = data.split('.')[0]        # room
+    else:
+        data = args.src
+        data_url = f'{base}/data/{data}'
+        name_0 = data.split('.')[0]
 
     name = name_0
     # 폴더명 중복 피하기
@@ -24,7 +44,7 @@ def main(args):
         uniq += 1
 
     # ns-process-data
-    os.system(f'source activate nerfstudio && ns-process-data video --data {base}/data/{data} --output-dir {base}/data/{name}')
+    os.system(f'source activate nerfstudio && ns-process-data video --data {data_url} --output-dir {base}/data/{name}')
     # TODO os.popopen(f'source activate nerfstudio && ns-process-data video --data {base}/data/{data} --output-dir {base}/data/{name}')
     get_matching_summary = ''   # TODO process-data에서 출력이나 flag 받아와야 함
     msg = f'{get_matching_summary} \
@@ -60,6 +80,13 @@ def main(args):
     msg = '공간 추출이 완료되었습니다!'
     # TODO 웹 서버로 msg 전송
 
+    print("web server로 전송 중")
+    send_start = time.time()
+    send(name, f'{output_dir}/exports/pcd_10000000_s_20/point_cloud.ply', args.id)
+    # send(name, '/home/cmj.gcp.2/nerfstudio/data/test/images_8/frame_00001.png', args.id) # NOTE request test
+    print(f"전송 완료. Elapsed time: {timedelta(seconds=time.time() - send_start)}")
+
+
 
 if __name__ == "__main__":
     # ----- Parser -----
@@ -81,10 +108,9 @@ if __name__ == "__main__":
     # parser = argparse.ArgumentParser()
     parser = argparse.ArgumentParser(parents=[cli_parser], add_help=False)
 
-    parser.add_argument('--data',
+    parser.add_argument('--src',
                         type=str, 
-                        help='(Required) user data name.',
-                        default="room3.mp4")
+                        help='(Required) user data name.')
     parser.add_argument('--model',
                         type=str, 
                         help='(Required) model.',
