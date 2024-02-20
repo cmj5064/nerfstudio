1b8791102f2e919d98e6423c8992b501120c9720
cmj5064 <duttu5064@naver.com>
2024-02-20 10:38:23 +0000

    fix: pipeline 도중 error 예외 처리를 위한 수정

--- pipe.py
+++ pipe.py
@@ -6,10 +6,12 @@ import json
 import requests
 from requests_toolbelt.multipart.encoder import MultipartEncoder
 import json
+import subprocess as sp
+import sys
 
 
 def send(name, file, id):
-    url = "https://zzimkong.ggm.kr/inference/done"
+    url = "https://zzimkong.ggm.kr/inference/upload"
     ply_file = MultipartEncoder(
         fields={
             'file': (f'{name}.ply', open(file, 'rb'))
@@ -17,27 +19,39 @@ def send(name, file, id):
     )
     data = {"file": ply_file, "id": id}
     r = requests.post(url, data=data, verify=False)
+    return r.status_code
 
+
+def status(status, message, id):
+    url = "https://zzimkong.ggm.kr/inference/status"
+    data = {"status": status, "statusMessage": message, "id": id}
+    r = requests.post(url, data=data, verify=False)
 
 def main(args):
     start = time.time()
     msg = '공간 영상 전처리 수행 중 \
         약 30분이 소요됩니다.'    # user에게 보여줄 메시지
-    # TODO 웹 서버로 msg 전송
+    status("progress", msg, args.id)
 
     base = os.getcwd()
     if 'nerfstudio' not in base:
-        base = f'{base}/nerfstudio' # pwd output. ./nerfstudio
-
+        base = f'{base}/nerfstudio'    # pwd output. ./nerfstudio
     model = args.model                 # nerfacto
-    if "https:" in args.src:
-        data_url = args.src                # https://zzimkong.ggm.kr/2024.mov
-        data = data_url.split('/')[-1]     # room.mp4
-        name_0 = data.split('.')[0]        # room
-    else:
-        data = args.src
-        data_url = f'{base}/data/{data}'
-        name_0 = data.split('.')[0]
+
+    # src로 url 들어올 시
+    # if "https:" in args.src:
+    #     data_url = args.src                # https://zzimkong.ggm.kr/2024.mov
+    #     data = data_url.split('/')[-1]     # room.mp4
+    #     name_0 = data.split('.')[0]        # room
+    # else:
+    #     data = args.src
+    #     data_url = f'{base}/data/{data}'
+    #     name_0 = data.split('.')[0]
+
+    # UXR까지는 src로 url이 아닌 파일명만
+    data_url = f'https://zzimkong.ggm.kr/{args.src}'                # https://zzimkong.ggm.kr/2024.mov
+    data = data_url.split('/')[-1]                                  # room.mp4
+    name_0 = data.split('.')[0]                                     # room
 
     name = name_0
     # 폴더명 중복 피하기
@@ -47,25 +61,33 @@ def main(args):
         uniq += 1
 
     # ns-process-data
-    os.system(f'source activate nerfstudio && ns-process-data video --data {data_url} --output-dir {base}/data/{name}')
-    # TODO os.popopen(f'source activate nerfstudio && ns-process-data video --data {base}/data/{data} --output-dir {base}/data/{name}')
+    command = f'source activate nerfstudio && ns-process-data video --data {data_url} --output-dir {base}/data/{name}'
+    s = sp.run(command, capture_output=False, text=True, shell=True)
+    if s.returncode != 0:
+        status("error", "데이터 전처리 중 에러가 발생하였습니다.", args.id)
+        os.abort()
+    # TODO os.popen(f'source activate nerfstudio && ns-process-data video --data {base}/data/{data} --output-dir {base}/data/{name}')
     get_matching_summary = ''   # TODO process-data에서 출력이나 flag 받아와야 함
     msg = f'{get_matching_summary} \
         공간 모델 학습 수행 중 \
         약 30분이 소요됩니다.'
-    # TODO 웹 서버로 msg 전송
+    status("progress", msg, args.id)
 
     # ns-train
-    os.system(f'source activate nerfstudio && ns-train {model} --data {base}/data/{name} --output-dir {base}/outputs --pipeline.model.predict-normals True --vis wandb')
+    command = f'source activate nerfstudio && ns-train {model} --data {base}/data/{name} --output-dir {base}/outputs --pipeline.model.predict-normals True --vis wandb'
+    s = sp.run(command, capture_output=False, text=True, shell=True)
+    if s.returncode != 0:
+        status("error", "공간 모델 학습 중 에러가 발생하였습니다.", args.id)
+        os.abort()
     msg = '3차원 공간 추출 중 \
         약 10분이 소요됩니다.'
-    # TODO 웹 서버로 msg 전송
+    status("progress", msg, args.id)
 
     outs_dir=f"{base}/outputs/{name}/{model}/"
     output_dir = outs_dir + sorted(os.listdir(outs_dir))[-1]
 
     # ns-export
-    os.system(f'source activate nerfstudio && ns-export pointcloud \
+    command = f'source activate nerfstudio && ns-export pointcloud \
     --load-config {output_dir}/config.yml \
     --output-dir {output_dir}/exports/pcd_10000000_s_20/ \
     --num-points 10000000 \
@@ -75,19 +97,26 @@ def main(args):
     --save-world-frame False \
     --obb_center 0.0000000000 0.0000000000 0.0000000000 \
     --obb_rotation 0.0000000000 0.0000000000 0.0000000000 \
-    --obb_scale 20.0000000000 20.0000000000 20.0000000000')
+    --obb_scale 20.0000000000 20.0000000000 20.0000000000'
+    s = sp.run(command, capture_output=False, text=True, shell=True)
+    if s.returncode != 0:
+        status("error", "공간 추출 중 에러가 발생하였습니다.", args.id)
+        os.abort()
 
     print("Point cloud exported!")
     print(f"Elapsed time: {timedelta(seconds=time.time() - start)}")
 
     msg = '공간 추출이 완료되었습니다!'
-    # TODO 웹 서버로 msg 전송
+    status("progress", msg, args.id)
 
     print("web server로 전송 중")
     send_start = time.time()
-    send(name, f'{output_dir}/exports/pcd_10000000_s_20/point_cloud.ply', args.id)
-    print(f"전송 완료. Elapsed time: {timedelta(seconds=time.time() - send_start)}")
-
+    result = send(name, f'{output_dir}/exports/pcd_10000000_s_20/point_cloud.ply', args.id)
+    if result == 201:
+        print(f"전송 완료. Elapsed time: {timedelta(seconds=time.time() - send_start)}")
+        status("progress", "ply 전송이 완료되었습니다.", args.id)
+    else:
+        status("error", "ply 전송에 실패하였습니다.", args.id)
 
 
 if __name__ == "__main__":
